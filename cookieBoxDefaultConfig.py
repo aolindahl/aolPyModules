85ee74396143bac5629ea1df15bb3901010e592e
Anton O. Lindahl <aolindahl@gmail.com>
2014-12-03 15:17:21 -0800

    Offline data should include everyhing. Correct exp now.

--- cookieBoxDefaultConfig.py
+++ cookieBoxDefaultConfig.py
@@ -95,7 +95,7 @@ fitMask = np.array([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])
 boolFitMask = np.array([i in fitMask for i in range(16)])
 nanFitMask = np.array([1 if b else np.nan for b in boolFitMask])
 
-offlineSource = 'exp=amoi0114:run=144'
+offlineSource = 'exp=amoi0314:run=10'
 
 # For CookieBox class debugging
 domainToDisplay = 'Time'
